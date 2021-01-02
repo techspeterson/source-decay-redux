bb3e1aeda3ec1c315bbcad78ea5c956f9371e49a
techspeterson <techspeterson@gmail.com>
2021-01-02 15:58:51 +1100

    topbar created

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import Topbar from "./Topbar";
+import './App.css';
+
+function Home() {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Topbar/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const toggleMenu = () => { }
+
+function Topbar() {
+  return (
+    <div id="topbar">
+      <hr className="hr-deco mobile-hide" />
+      <div className="menu mobile-show">
+        <i className="fas fa-bars menu-icon" onClick={toggleMenu}></i>
+        <div id="menu-inner" className="container-box">
+          <div id="description">
+            <hr />
+            description here
+          </div>
+
+
+          <ul className="linkbox">
+            <li><a href="/">index</a></li>
+          </ul>
+          <hr />
+          <form action="/search" method="get" className="search">
+            <input type="text" name="q" value="{SearchQuery}" className="search-input" />
+            <button type="submit" className="search-submit"><span className="fas fa-search" alt="Search"></span></button>
+          </form>
+        </div>
+
+      </div>
+      <div className="corner-small corner-bottom-right"></div>
+      <a href="/"><div id="topbar-inner">
+        <h1>Source Decay</h1>
+      </div></a>
+      <div className="corner-small corner-bottom-left"></div>
+      <hr className="hr-deco" />
+    </div>
+  )
+}
+
+export default Topbar;
